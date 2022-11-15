210e72cda39476c80f33fdb7b360c8a96cb1bdb1
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-15 18:59:20 +0100

    initialized spring project

---
+++ teo-pet-clinic/src/main/java/mk/kalitek/teopetclinic/TeoPetClinicApplication.java
@@ -0,0 +1,13 @@
+package mk.kalitek.teopetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeoPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeoPetClinicApplication.class, args);
+	}
+
+}
---
+++ teo-pet-clinic/src/test/java/mk/kalitek/teopetclinic/TeoPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.kalitek.teopetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeoPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
