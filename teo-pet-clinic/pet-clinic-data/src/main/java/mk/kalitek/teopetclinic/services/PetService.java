a0e14b6a4e00e4f9012c1a24d74c02a2814f067e
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-26 16:36:03 +0100

    Adding Crud Service. Closes #11

--- teo-pet-clinic/pet-clinic-data/src/main/java/mk/kalitek/teopetclinic/services/PetService.java
+++ teo-pet-clinic/pet-clinic-data/src/main/java/mk/kalitek/teopetclinic/services/PetService.java
@@ -2,14 +2,6 @@ package mk.kalitek.teopetclinic.services;
 
 import mk.kalitek.teopetclinic.model.Pet;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 
 }
--- teo-pet-clinic/pet-clinic-data/src/main/java/mk/kalitek/teopetclinic/services/VetService.java
+++ teo-pet-clinic/pet-clinic-data/src/main/java/mk/kalitek/teopetclinic/services/VetService.java
@@ -2,14 +2,6 @@ package mk.kalitek.teopetclinic.services;
 
 import mk.kalitek.teopetclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
