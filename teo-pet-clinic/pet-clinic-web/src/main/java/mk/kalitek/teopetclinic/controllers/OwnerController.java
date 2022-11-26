0863b37a5a445c8a0a0be8c51f0884f8535fe89d
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-26 18:13:34 +0100

    Added owner html page and owner controller. Closes #14

---
+++ teo-pet-clinic/pet-clinic-web/src/main/java/mk/kalitek/teopetclinic/controllers/OwnerController.java
@@ -0,0 +1,16 @@
+package mk.kalitek.teopetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/owners")
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"", "index", "index.html"})
+    public String index() {
+
+        return "owners/index";
+    }
+
+}
