52df6da6f49aa0f14b05f7939b986e152ce92ee6
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-26 18:08:04 +0100

    Added vet html page and vet controller. Closes #7

---
+++ teo-pet-clinic/pet-clinic-web/src/main/java/mk/kalitek/teopetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package mk.kalitek.teopetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+
+        return "vets/index";
+    }
+
+}
