dad80a6a11d02b21a0164264997be8b0f8ae8d5d
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-26 16:52:48 +0100

    Implemented map based services. Closes #6

---
+++ teo-pet-clinic/pet-clinic-data/src/main/java/mk/kalitek/teopetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package mk.kalitek.teopetclinic.services.map;
+
+import mk.kalitek.teopetclinic.model.Vet;
+import mk.kalitek.teopetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap  extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+}
