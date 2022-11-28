6d70bb714312e0f27fd623ea15dc45d12194ce26
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-28 19:46:46 +0100

    Added Map implementation to manage setting of Autogenerated ID properties. Closes #24

--- teo-pet-clinic/pet-clinic-web/src/main/java/mk/kalitek/teopetclinic/bootstrap/DataLoader.java
+++ teo-pet-clinic/pet-clinic-web/src/main/java/mk/kalitek/teopetclinic/bootstrap/DataLoader.java
@@ -25,14 +25,12 @@ public class DataLoader implements CommandLineRunner {
 
 //        owner loading
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Westen");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glennane");
 
@@ -42,14 +40,12 @@ public class DataLoader implements CommandLineRunner {
 
 //        vet loading
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
 
